87f9776a0b46ac500eb0d80d229b0bf1d92b7cd0
Varzoer <konorotip@gmail.com>
2022-09-03 19:30:07 +0300

    burger menu completed

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,6 +1,14 @@
-const hamburger = document.getElementById('hamburger');
-const navDiv = document.getElementById("nav-div");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener('click', () => {
-    navDiv.classList.toggle('show')
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
